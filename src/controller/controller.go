cf3be539f336cdbf95a507e2e2a502f9c5db4b22
Emilio Pomares <remilia@Emilios-iMac.home>
2019-06-04 13:01:21 +0200

    Latest

--- src/controller/controller.go
+++ src/controller/controller.go
@@ -1,18 +1,19 @@
 package main
 
 import (
+	"context"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
 	"os"
 	"strconv"
-	"log"
-	"fmt"
-	"net/http"
-	"context"
+
 	"github.com/gorilla/mux"
 
 	"go.mongodb.org/mongo-driver/bson"
-        "go.mongodb.org/mongo-driver/mongo"
-        "go.mongodb.org/mongo-driver/mongo/options"
-
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 var db_svr string
@@ -24,27 +25,30 @@ var shrinkThreshold float32 = 0.4
 // Types for db documents
 
 type Status struct {
-	NumberOfCells  int  `json:"numberofcells"`
+	NumberOfCells  int    `json:"numberofcells"`
 	TotalSpace     uint64 `json:"totalspace"`
-	CellNamePrefix	string `json:"cellnameprefix"`
+	CellNamePrefix string `json:"cellnameprefix"`
+	UsedSpace      uint64 `json:"usedspace"`
 }
 
 type CellStatus struct {
-	CellId		int `json:"_id"`
-	FreeSpace	uint64 `json:"freespace"`
-	NumberOfFiles   uint64 `json:"numberoffile"`
+	CellId        int    `json:"_id"`
+	Capacity      uint64 `json:"capacity"`
+	FreeSpace     uint64 `json:"freespace"`
+	NumberOfFiles uint64 `json:"numberoffile"`
 }
 
 type Directory struct {
-	Collection	string `json:"collection"`
-	Path		string `json:"path"`
-	CellId		int	`json:"cellid"`
+	Category string `json:"category"`
+	Path     string `json:"path"`
+	CellId   int    `json:"cellid"`
 }
 
 type DBConnectionContext struct {
-        client	                 *mongo.Client
-	serverstatus		 *mongo.Collection
-	cellstatus		 *mongo.Collection
+	client       *mongo.Client
+	serverstatus *mongo.Collection
+	cellstatus   *mongo.Collection
+	directories  *mongo.Collection
 }
 
 var dbConnectionContext DBConnectionContext
@@ -53,19 +57,19 @@ var dbConnectionContext DBConnectionContext
 
 func getServerStatus(conn *DBConnectionContext) (Status, error) {
 	var statusInDB Status
-        err := conn.serverstatus.FindOne(context.TODO(), bson.D{{"_id", 0}}).Decode(&statusInDB)
-  	return statusInDB, err
+	err := conn.serverstatus.FindOne(context.TODO(), bson.D{{"_id", 0}}).Decode(&statusInDB)
+	return statusInDB, err
 }
 
 func initializeServerStatus(conn *DBConnectionContext) error {
 	cellcapacity := InitializeNewCell()
-	_, err := conn.serverstatus.InsertOne(context.TODO(), bson.D{{"_id", 0}, 
+	_, err := conn.serverstatus.InsertOne(context.TODO(), bson.D{{"_id", 0},
 		{"numberofcells", 1}, {"totalspace", cellcapacity}, {"cellnameprefix", "storagecell"}})
 	if err != nil {
 		return err
 	}
 	_, err = conn.cellstatus.InsertOne(context.TODO(), bson.D{{"_id", 0},
-                {"freespace", cellcapacity}, {"numberoffiles", 0} })
+		{"freespace", cellcapacity}, {"capacity", cellcapacity}, {"numberoffiles", 0}})
 	return err
 }
 
@@ -74,50 +78,109 @@ func HealthCheck(w http.ResponseWriter, r *http.Request) {
 }
 
 func connectToDB() (*mongo.Client, error) {
-        clientOptions := options.Client().ApplyURI("mongodb://" + db_svr + ":" + db_port)
-        return mongo.Connect(context.TODO(), clientOptions)
+	clientOptions := options.Client().ApplyURI("mongodb://" + db_svr + ":" + db_port)
+	return mongo.Connect(context.TODO(), clientOptions)
 }
 
 // Utils
+
+// Utilities
+
+func JSONResponseFromString(w http.ResponseWriter, res string) {
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	w.WriteHeader(http.StatusOK)
+	io.WriteString(w, res)
+}
 
 func InitializeNewCell() uint64 {
 	return 100
 }
 
-func findCellWithFreeSpace(requestedSpace uint64) int {
-    var results []client
+func addDirectoryEntry(conn *DBConnectionContext, category string, fullpath string, cellid int) error {
+	_, err := conn.directories.InsertOne(context.TODO(), bson.D{
+		{"category", category}, {"path", fullpath}, {"cellid", cellid}})
+	return err
+}
 
-    err := db.C("client").Find(nil).All(&results)
-    if err != nil {
-        // TODO: Do something about the error
-    } else {
-        fmt.Println("Results All: ", results) 
-    }
+func findCellWithFreeSpace(conn *DBConnectionContext, requestedSpace uint64) int {
+
+	var results []*CellStatus
+
+	cursor, err := conn.cellstatus.Find(context.TODO(), bson.D{{}})
+
+	if err != nil {
+		return -2
+	} else {
+
+		for cursor.Next(context.TODO()) {
+			var elem CellStatus
+			err := cursor.Decode(&elem)
+			if err != nil {
+				return -3
+			}
+			results = append(results, &elem)
+		}
+
+		cursor.Close(context.TODO())
+
+		for _, element := range results {
+			fmt.Println(element)
+		}
+
+		return 0
+
+	}
+}
+
+func addUsedStorage(conn *DBConnectionContext, amount uint64, cellid int) {
+	_, err := conn.serverstatus.UpdateOne(context.TODO(), bson.D{{"_id", 0}}, bson.D{{"$inc", bson.D{{"usedspace", amount}}}})
+	if(err != nil) {
+		fmt.Println(err)
+	}
+	_, err = conn.cellstatus.UpdateOne(context.TODO(), bson.D{{"_id", cellid}}, bson.D{{"$inc", bson.D{{"freespace", -int64(amount)}}}})
+	if(err != nil) {
+		fmt.Println(err)
+	}
+	_, err = conn.cellstatus.UpdateOne(context.TODO(), bson.D{{"_id", cellid}}, bson.D{{"$inc", bson.D{{"numberoffiles", 1}}}})
+	if(err != nil) {
+		fmt.Println(err)
+	}
 }
 
 // REST API Functions
 
 func Retrieve(w http.ResponseWriter, r *http.Request) {
-        vars := mux.Vars(r)
-        fmt.Println("Attempting to retrieve value " + vars["value"])
+	vars := mux.Vars(r)
+	fmt.Println("Attempting to retrieve value " + vars["value"])
 }
 
 func Store(w http.ResponseWriter, r *http.Request) {
-        vars := mux.Vars(r)
-        fmt.Println("Attempting to retrieve value " + vars["value"])
-	lengthOfValue := len(vars["value"])
-	cellid := findCellWithFreeSpace(lengthOfValue)
-	if(cellid == -1) {
-		
+	vars := mux.Vars(r)
+	fmt.Println("Attempting to retrieve value " + vars["value"])
+	lengthOfValue := uint64(len(vars["value"]))
+	cellid := findCellWithFreeSpace(&dbConnectionContext, lengthOfValue)
+	if cellid == -1 {
+		JSONResponseFromString(w, "{\"result\":\"'Try later'\"}")
 	} else {
-
+		fmt.Println("Storing data in cell " + strconv.Itoa(cellid))
+		addDirErr := addDirectoryEntry(&dbConnectionContext, "default", vars["key"], cellid)
+		if addDirErr != nil {
+			JSONResponseFromString(w, "{\"result\":\"'Server error'\"}")
+		} else {
+			addUsedStorage(&dbConnectionContext, lengthOfValue, cellid)
+			JSONResponseFromString(w, "{\"result\":\"'OK'\", \"bytes\":"+strconv.FormatUint(lengthOfValue, 10)+"}")
+		}
 	}
+}
+
+func GetServiceStatus(w http.ResponseWriter, r *http.Request) {
+
 }
 
 func main() {
 
 	ControllerPort := "2222"
-	
+
 	db_svr = os.Getenv("DB_SVR")
 	db_port = os.Getenv("DB_PORT")
 	if db_svr == "" {
@@ -129,17 +192,18 @@ func main() {
 
 	fmt.Println("Trying to connecto to " + db_svr + ":" + db_port + "...")
 	client, err := connectToDB()
-        err = client.Ping(context.TODO(), nil)
+	err = client.Ping(context.TODO(), nil)
 
-        if err != nil {
-                log.Fatal(err)
-        }
+	if err != nil {
+		log.Fatal(err)
+	}
 
-        fmt.Println("Connected to MongoDB!")
+	fmt.Println("Connected to MongoDB!")
 
 	dbConnectionContext.client = client
 	dbConnectionContext.serverstatus = client.Database("service").Collection("serverstatus")
 	dbConnectionContext.cellstatus = client.Database("service").Collection("cellstatus")
+	dbConnectionContext.directories = client.Database("service").Collection("directories")
 
 	stat, staterr := getServerStatus(&dbConnectionContext)
 
@@ -154,8 +218,9 @@ func main() {
 	r.HandleFunc("/healthcheck", HealthCheck).Methods("GET")
 	r.HandleFunc("/{key}/{value}", Store).Methods("PUT")
 	r.HandleFunc("/{key}", Retrieve).Methods("GET")
+	r.HandleFunc("/status", GetServiceStatus).Methods("GET")
 
-	if err := http.ListenAndServe(":" + ControllerPort, r); err != nil {
+	if err := http.ListenAndServe(":"+ControllerPort, r); err != nil {
 		log.Fatal(err)
 	}
 
